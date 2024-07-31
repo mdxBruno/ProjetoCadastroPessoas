41a43fa1f584481ccde9d993a0b9f6c1dac3938f
Bruno Tavares <125074529+mdxBruno@users.noreply.github.com>
2024-07-31 13:58:29 -0300

    Add files via upload

---
+++ CadastroPessoas/CadastroPessoas/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CadastroPessoas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroPessoas.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    DbSet<Pessoa> Pessoas { get; set; } = null;
+}
---
+++ CadastroPessoas/CadastroPessoas/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace CadastroPessoas.Models;
+
+public class Pessoa
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Cpf { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+}
